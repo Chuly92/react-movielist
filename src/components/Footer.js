3bac50c6e6ea8bd30e069fa9e0d74eaf5c310566
Julieta Gallego <julietagallego92@gmail.com>
2022-03-24 13:42:29 -0300

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Footer = () => {
-  return (
-      <footer className="footer container small text-muted">
-        <p className="text-center">© Copyright Alkemy Challenge</p>
-      </footer>
-  )
-}
